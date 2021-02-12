c743548ad1f545c5da3ac42973a600ab9d15a5d1
emonzon42 <elijah.monzon42@gmail.com>
2021-02-11 22:03:28 -0500

    created xcode project

---
+++ LocaNotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocaNotes
+//
+//  Created by Elijah Monzon on 2/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocaNotes/LocaNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaNotesApp.swift
+//  LocaNotes
+//
+//  Created by Elijah Monzon on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LocaNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
