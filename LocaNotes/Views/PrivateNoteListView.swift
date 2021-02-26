f57ab095a18bee9c37b8e185cf2a67bc7b89a705
acomanzo <ansonih17@gmail.com>
2021-02-25 23:38:07 -0500

    made view to represent the list of private notes

--- LocaNotes/Views/PrivateNoteListView.swift
+++ LocaNotes/Views/PrivateNoteListView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct PrivateNoteListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                HStack {
+                    
+                }
+            }
+        }
     }
 }
 
