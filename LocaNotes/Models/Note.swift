864e6cb63313d6a0f6f63d1668b64dc6f7f1b5e4
acomanzo <ansonih17@gmail.com>
2021-02-25 23:37:05 -0500

    made model to represent the Note table

--- LocaNotes/Models/Note.swift
+++ LocaNotes/Models/Note.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Note {
+    let id: Int32
+    let userId: Int32
+    let longitude: NSString
+    let latitude: NSString
+    let timestamp: Int32 // unix
+    let body: NSString
+}
+
+extension Note: SQLTable {
+    static var createStatement: String {
+        return """
+            CREATE TABLE Note(
+                NoteId INT NOT NULL INDENTITY PRIMARY KEY,
+                UserId INT NOT NULL FOREIGN KEY REFERENCES User(UserId),
+                Latitude VARCHAR(10) NOT NULL,
+                Longitude VARCHAR(10) NOT NULL,
+                Timestamp INT NOT NULL,
+                Body VARCHAR(500) NOT NULL
+            );
+        """
+    }
+}
