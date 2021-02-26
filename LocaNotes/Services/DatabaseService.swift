ebfbc09b47fb7dcd5b09683df6bf095376d98737
acomanzo <ansonih17@gmail.com>
2021-02-25 23:37:29 -0500

    made service to help access the database

--- LocaNotes/Services/DatabaseService.swift
+++ LocaNotes/Services/DatabaseService.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import SQLite3
+
+class DatabaseService {
+    
+    private var db: SQLiteDatabase!
+    
+    init() {
+        
+        do {
+            let path: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first ?? ""
+
+            db = try SQLiteDatabase.open(path: "\(path)/locanotes_db.sqlite3")
+            print("Opened connection to database: \(path)/locanotes_db.sqlite3")
+            
+            if (!UserDefaults.standard.bool(forKey: "is_db_created")) {
+                
+                do {
+                    // create User table
+                    try db.createTable(table: User.self)
+                    
+                    // create Note table
+                    try db.createTable(table: Note.self)
+                } catch {
+                    print(db.errorMessage)
+                }
+                
+                UserDefaults.standard.set(true, forKey: "is_db_created")
+            }
+        } catch SQLiteError.OpenDatabase(_) {
+            print("Unable to open database.")
+        } catch {
+            print("Unable to open database: \(error.localizedDescription)")
+        }
+    }
+}
+
+
