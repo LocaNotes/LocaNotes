f039b98e20aef702ca73afbea07fba98adc1a3ee
acomanzo <ansonih17@gmail.com>
2021-02-25 23:36:51 -0500

    made model to represent the User table

--- LocaNotes/Models/User.swift
+++ LocaNotes/Models/User.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct User {
+    let id: Int32
+    let firstName: NSString
+    let lastName: NSString
+    let email: NSString
+    let username: NSString
+    let password: NSString
+}
+
+extension User: SQLTable {
+    static var createStatement: String {
+        return """
+            CREATE TABLE User(
+                UserId INT NOT NULL IDENTITY PRIMARY KEY,
+                FirstName VARCHAR(20) NOT NULL,
+                LastName VARCHAR(20) NOT NULL,
+                Email VARCHAR(40) NOT NULL,
+                Username VARCHAR(40) NOT NULL,
+                Password VARCHAR(40) NOT NULL
+            );
+        """
+    }
+}
