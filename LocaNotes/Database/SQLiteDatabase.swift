55832b459de6ba05ae4c0eb1b650fcdb7528cd8c
acomanzo <ansonih17@gmail.com>
2021-03-01 00:42:12 -0500

    added insert and delete functions

--- LocaNotes/Database/SQLiteDatabase.swift
+++ LocaNotes/Database/SQLiteDatabase.swift
@@ -13,6 +13,7 @@ enum SQLiteError: Error {
     case Prepare(message: String)
     case Step(message: String)
     case Bind(message: String)
+    case Delete(message: String)
 }
 
 class SQLiteDatabase {
@@ -59,6 +60,7 @@ class SQLiteDatabase {
 }
 
 extension SQLiteDatabase {
+    
     func prepareStatement(sql: String) throws -> OpaquePointer? {
         var statement: OpaquePointer?
         guard sqlite3_prepare_v2(dbPointer, sql, -1, &statement, nil) == SQLITE_OK else {
@@ -86,4 +88,98 @@ extension SQLiteDatabase {
 
 protocol SQLTable {
     static var createStatement: String { get }
+}
+
+extension SQLiteDatabase {
+    
+    func queryAllNotes() -> [Note]? {
+        let querySql  = "SELECT * FROM Note;"
+        guard let queryStatement = try? prepareStatement(sql: querySql) else {
+            return nil
+        }
+        defer {
+            sqlite3_finalize(queryStatement)
+        }
+        
+        var notes: [Note] = []
+        while (sqlite3_step(queryStatement) == SQLITE_ROW) {
+            // get note id
+            let noteId = sqlite3_column_int(queryStatement, 0)
+            
+            // get user id
+            let userId = sqlite3_column_int(queryStatement, 1)
+            
+            // get longitude
+            guard let queryResultCol2 =  sqlite3_column_text(queryStatement, 2) else {
+                print("Query result is nil")
+                return nil
+            }
+            let longitude = String(cString: queryResultCol2) as NSString
+            
+            // get latitude
+            guard let queryResultCol3 =  sqlite3_column_text(queryStatement, 3) else {
+                print("Query result is nil")
+                return nil
+            }
+            let latitude = String(cString: queryResultCol3) as NSString
+            
+            // get timestamp
+            let timestamp = sqlite3_column_int(queryStatement, 4)
+            
+            // get body
+            guard let queryResultCol5 =  sqlite3_column_text(queryStatement, 5) else {
+                print("Query result is nil")
+                return nil
+            }
+            let body = String(cString: queryResultCol5) as NSString
+            
+            notes.append(Note(noteId: noteId, userId: userId, longitude: longitude, latitude: latitude, timestamp: timestamp, body: body))
+        }
+        return notes
+    }
+}
+
+extension SQLiteDatabase {
+    
+    func deleteNoteById(id: Int32) throws {
+        
+        let deleteStatementString = "DELETE FROM Note WHERE NoteId = ?;"
+                
+        guard let deleteStatement = try? prepareStatement(sql: deleteStatementString) else {
+            throw SQLiteError.Prepare(message: errorMessage)
+        }
+        defer {
+            sqlite3_finalize(deleteStatement)
+        }
+        
+        // bind the id to the statement (1-based index)
+        sqlite3_bind_int(deleteStatement, 1, id)
+        
+        if sqlite3_step(deleteStatement) != SQLITE_DONE {
+            throw SQLiteError.Delete(message: errorMessage)
+        }
+    }
+}
+
+extension SQLiteDatabase {
+    
+    func insertNote(userId: Int32, latitude: String, longitude: String, timestamp: Int32, body: String) throws {
+        let insertSql = "INSERT INTO Note (UserId, Latitude, Longitude, Timestamp, Body) VALUES (?, ?, ?, ?, ?);"
+        let insertStatement = try prepareStatement(sql: insertSql)
+        defer {
+            sqlite3_finalize(insertStatement)
+        }
+        guard
+            sqlite3_bind_int(insertStatement, 1, userId) == SQLITE_OK &&
+            sqlite3_bind_text(insertStatement, 2, latitude, -1, nil) == SQLITE_OK &&
+            sqlite3_bind_text(insertStatement, 3, longitude, -1, nil) == SQLITE_OK &&
+            sqlite3_bind_int(insertStatement, 4, timestamp) == SQLITE_OK &&
+            sqlite3_bind_text(insertStatement, 5, body, -1, nil) == SQLITE_OK
+        else {
+            throw SQLiteError.Bind(message: errorMessage)
+        }
+        guard sqlite3_step(insertStatement) == SQLITE_DONE else {
+            throw SQLiteError.Step(message: errorMessage)
+        }
+    }
 }
