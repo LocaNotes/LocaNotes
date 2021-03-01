6c4714e48a888e9b6bf2d0d88633c73e82c3a9c0
acomanzo <ansonih17@gmail.com>
2021-03-01 00:43:20 -0500

    created class to provide push notifications

--- LocaNotes/Services/NotificationService.swift
+++ LocaNotes/Services/NotificationService.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UserNotifications
+
+class NotificationService {
+    let center: UNUserNotificationCenter
+    
+    init() {
+        center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+            // handle denial in this closure
+        }
+    }
+}
