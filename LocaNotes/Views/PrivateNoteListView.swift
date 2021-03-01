e415c3c410234e6807be30709129d881f18b6cd0
acomanzo <ansonih17@gmail.com>
2021-03-01 00:45:04 -0500

    updated the private note list view so that it'll actually show notes

--- LocaNotes/Views/PrivateNoteListView.swift
+++ LocaNotes/Views/PrivateNoteListView.swift
@@ -8,19 +8,50 @@
 import SwiftUI
 
 struct PrivateNoteListView: View {
+        
+    @ObservedObject var viewModel: NoteViewModel
+    
+    init (viewModel: NoteViewModel) {
+        self.viewModel = viewModel
+    }
+    
     var body: some View {
         NavigationView {
-            VStack {
-                HStack {
-                    
+            List {
+                ForEach(viewModel.notes, id: \.noteId) { note in
+                    NoteCell(note: note)
                 }
+                .onDelete(perform: viewModel.deleteNote)
             }
+            .navigationTitle("private notes")
+            .navigationBarItems(leading: EditButton(), trailing: Text("Test"))
         }
+        .onAppear(perform: viewModel.refresh)
     }
 }
 
 struct PrivateNoteListView_Previews: PreviewProvider {
     static var previews: some View {
-        PrivateNoteListView()
+        PrivateNoteListView(viewModel: NoteViewModel())
+    }
+}
+
+struct NoteCell: View {
+    
+    let note: Note
+    
+    var body: some View {
+        NavigationLink(destination: PrivateNoteDetailView(note: note)) {
+            HStack {
+                Text(String(note.userId))
+                Text(substring(string: String(note.body), offset: 10))
+            }
+        }
+    }
+    
+    private func substring(string: String, offset: Int) -> String.SubSequence {
+        let index = string.index(string.startIndex, offsetBy: offset)
+        let substring = string[..<index]
+        return substring
     }
 }
