e1302a21b0bba644d87c4f5cbe00fa952cc538f0
acomanzo <ansonih17@gmail.com>
2021-03-01 00:44:05 -0500

    updated the ContentView to contain a tab bar to switch between views

--- LocaNotes/Views/ContentView.swift
+++ LocaNotes/Views/ContentView.swift
@@ -8,9 +8,102 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    @State var selectedIndex = 0
+//    @State var shouldShowModel = false
+//
+//    let tabBarImageNames = ["person", "gear", "plus.app.fill", "pencil", "lasso"]
+    
+    let noteViewModel = NoteViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            PrivateNoteListView(viewModel: noteViewModel)
+                .tabItem {
+                    Image(systemName: "homekit")
+                    Text("Home")
+                }
+            Text("Social")
+                .font(.system(size: 30, weight: .bold))
+                .tabItem {
+                    Image(systemName: "globe")
+                    Text("Social")
+                }
+            CreateNoteView(noteViewModel: noteViewModel)
+                .tabItem {
+                    Image(systemName: "pencil")
+                    Text("New Note")
+                }
+            Text("Settings")
+                .font(.system(size: 30, weight: .bold))
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+            Text("Account")
+                .font(.system(size: 30, weight: .bold))
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Account")
+                }
+        }
+        
+//        VStack(spacing: 0) {
+//
+//            ZStack {
+//
+//                Spacer()
+//                    .fullScreenCover(isPresented: $shouldShowModel, content: {
+//                        Button(action: {shouldShowModel.toggle()}, label: {
+//                            Text("Fullscreen cover")
+//                        })
+//                    })
+//
+//                switch selectedIndex {
+//                case 0:
+//                    PrivateNoteListView()
+//                case 1:
+//                    ScrollView {
+//                        Text("test")
+//                    }
+//                default:
+//                    // need navigation view so that the text is flush against the bottom
+//                    NavigationView {
+//                        Text("remaining tabs")
+//                    }
+//                }
+//            }
+//
+//            Divider()
+//                .padding(.bottom, 12)
+//
+//            HStack {
+//                ForEach(0..<5) { num in
+//                    Button(action: {
+//
+//                        if num == 2 {
+//                            shouldShowModel.toggle()
+//                            return
+//                        }
+//
+//                        selectedIndex = num
+//                    }, label: {
+//                        Spacer()
+//
+//                        if num == 2 {
+//                            Image(systemName: tabBarImageNames[num])
+//                                .font(.system(size: 44, weight: .bold))
+//                                .foregroundColor(.red)
+//                        } else {
+//                            Image(systemName: tabBarImageNames[num])
+//                                .font(.system(size: 24, weight: .bold))
+//                                .foregroundColor(selectedIndex == num ? Color(.label) : Color(white: 0.8))
+//                        }
+//                        Spacer()
+//                    })
+//                }
+//            }
+//        }
     }
 }
 
@@ -19,3 +112,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
