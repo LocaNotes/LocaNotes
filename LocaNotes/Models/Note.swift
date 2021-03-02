aed50e9ea7573c8de669d1e4b5694cab7a7f5871
acomanzo <ansonih17@gmail.com>
2021-03-01 19:49:22 -0500

    Update Note.swift
    fixed a bug where longitude was getting mapped to latitude and vice versa

--- LocaNotes/Models/Note.swift
+++ LocaNotes/Models/Note.swift
@@ -10,22 +10,33 @@ import Foundation
 struct Note {
     let noteId: Int32
     let userId: Int32
-    let longitude: NSString
     let latitude: NSString
+    let longitude: NSString
     let timestamp: Int32 // unix
     let body: NSString
 }
 
 extension Note: SQLTable {
     static var createStatement: String {
+//        return """
+//            CREATE TABLE Note(
+//                NoteId INTEGER NOT NULL PRIMARY KEY,
+//                UserId INTEGER NOT NULL,
+//                Latitude VARCHAR(20) NOT NULL,
+//                Longitude VARCHAR(20) NOT NULL,
+//                Timestamp INT NOT NULL,
+//                Body VARCHAR(500) NOT NULL,
+//                FOREIGN KEY(UserId) REFERENCES User(UserId)
+//            );
+//        """
         return """
             CREATE TABLE Note(
                 NoteId INTEGER NOT NULL PRIMARY KEY,
                 UserId INTEGER NOT NULL,
-                Latitude VARCHAR(10) NOT NULL,
-                Longitude VARCHAR(10) NOT NULL,
-                Timestamp INT NOT NULL,
-                Body VARCHAR(500) NOT NULL,
+                Latitude TEXT NOT NULL,
+                Longitude TEXT NOT NULL,
+                Timestamp INTEGER NOT NULL,
+                Body TEXT NOT NULL,
                 FOREIGN KEY(UserId) REFERENCES User(UserId)
             );
         """
