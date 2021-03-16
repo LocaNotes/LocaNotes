0b4d1570a7ad179a1a4391d2f015ad27e6e682cc
acomanzo <ansonih17@gmail.com>
2021-03-16 00:52:59 -0400

    create model to represent the user in the mongo db

---
+++ LocaNotes/Models/MongoUser.swift
@@ -0,0 +1,15 @@
+//
+//  MongoUser.swift
+//  LocaNotes
+//
+//  Created by Anthony C on 3/16/21.
+//
+
+import Foundation
+
+/**
+ Represents a user from the MongoDB database
+ */
+struct MongoUser: Codable {
+    
+}
