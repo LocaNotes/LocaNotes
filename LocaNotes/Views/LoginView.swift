78e9d4fda4fb217f90af8b2e6aea2f866310208e
acomanzo <ansonih17@gmail.com>
2021-03-16 02:46:52 -0400

    connected login view to sign up function

--- LocaNotes/Views/LoginView.swift
+++ LocaNotes/Views/LoginView.swift
@@ -14,6 +14,9 @@ struct LoginView: View {
     var body: some View {
         NavigationView {
             if logged {
+                MainView()
+                    .navigationBarHidden(true)
+            } else {
                 ZStack {
                     LinearGradient(gradient: .init(colors: [Color("Color"), Color("Color-1"), Color("Color-2")]), startPoint: .top, endPoint: .bottom)
                         .edgesIgnoringSafeArea(.all)
@@ -25,9 +28,6 @@ struct LoginView: View {
                         }
                     }
                 }
-            } else {
-                MainView()
-                    .navigationBarHidden(true)
             }
         }
     }
@@ -94,18 +94,6 @@ struct Home: View {
                 }
                 .padding(.top, 20)
             }
-            
-            HStack(spacing: 15) {
-                Color.white.opacity(0.7)
-                    .frame(width: 35, height: 1)
-                Text("or")
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                Color.white.opacity(0.7)
-                    .frame(width: 35, height: 1)
-                
-            }
-            .padding(.top, 10)
         }
         .padding()
     }
@@ -177,37 +165,68 @@ struct Login: View {
         }
     }
     
-    private func authenticateCallback(user: MongoUser?, error: Error?) {
+    private func authenticateCallback(mongoUser: MongoUser?, error: Error?) {
         if error != nil {
             restResponse = "\(error!)"
             didReceiveRestError.toggle()
         } else {
+            if self.user == "" {
+                self.user = self.mail
+            }
             logged.toggle()
         }
     }
     
     private func authenticateUser() {
-        if user == "" {
-            user = self.mail
-        }
         
         let restService = RESTService()
-        restService.authenticateUser(username: self.mail, password: self.pass, completion: authenticateCallback(user:error:))
+        restService.authenticateUser(username: self.mail, password: self.pass, completion: authenticateCallback(mongoUser:error:))
         
     }
 }
 
 struct SignUp: View {
+    @State var firstName = ""
+    @State var lastName = ""
     @State var mail = ""
+    @State var username = ""
     @State var pass = ""
     @State var repass = ""
+    
+    @AppStorage("stored_User") var user = ""
+    @AppStorage("status") var logged = false
+    
     var body: some View {
         VStack {
             VStack {
+                
+                HStack(spacing: 15) {
+                    Image(systemName: "person")
+                        .foregroundColor(.black)
+                    TextField("First name", text: self.$firstName)
+                }
+                .padding(.vertical, 20)
+                
+                HStack(spacing: 15) {
+                    Image(systemName: "person.3")
+                        .foregroundColor(.black)
+                    TextField("Last name", text: self.$lastName)
+                }
+                .padding(.vertical, 20)
+                
                 HStack(spacing: 15) {
                     Image(systemName: "envelope")
                         .foregroundColor(.black)
-                    TextField("Enter email address", text: self.$mail)
+                    TextField("Email address", text: self.$mail)
+                        .autocapitalization(.none)
+                }
+                .padding(.vertical, 20)
+                
+                HStack(spacing: 15) {
+                    Image(systemName: "person.crop.circle")
+                        .foregroundColor(.black)
+                    TextField("Username", text: self.$username)
+                        .autocapitalization(.none)
                 }
                 .padding(.vertical, 20)
                 
@@ -219,7 +238,8 @@ struct SignUp: View {
                         .frame(width: 15, height: 18)
                         .foregroundColor(.black)
                     
-                    SecureField("Enter password", text: self.$pass)
+                    SecureField("Password", text: self.$pass)
+                        .autocapitalization(.none)
                     
                     Button(action: {
                         
@@ -239,6 +259,7 @@ struct SignUp: View {
                         .foregroundColor(.black)
                     
                     SecureField("Re-Enter password", text: self.$repass)
+                        .autocapitalization(.none)
                     
                     Button(action: {
                         
@@ -257,7 +278,7 @@ struct SignUp: View {
             .padding(.top, 25)
             
             Button(action: {
-                
+                createUser()
             }) {
                 Text("SIGN UP")
                     .foregroundColor(.white)
@@ -271,6 +292,21 @@ struct SignUp: View {
             .padding(.bottom, -40)
             .shadow(radius: 15)
         }
+    }
+    
+    private func createUserCallback(user: MongoUser?, error: Error?) {
+        if error != nil {
+            
+        } else {
+            self.user = self.mail
+            logged.toggle()
+            print("created user")
+        }
+    }
+    
+    private func createUser() {
+        let restService = RESTService()
+        restService.createUser(firstName: firstName, lastName: lastName, email: mail, username: username, password: pass, completion: createUserCallback(user:error:))
     }
 }
 
