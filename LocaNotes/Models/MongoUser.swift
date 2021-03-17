ed4e465cb62717ea88c71606cfba7d2b3db31bd0
acomanzo <ansonih17@gmail.com>
2021-03-17 03:56:47 -0400

    updated mongo user

--- LocaNotes/Models/MongoUser.swift
+++ LocaNotes/Models/MongoUser.swift
@@ -11,5 +11,13 @@ import Foundation
  Represents a user from the MongoDB database
  */
 struct MongoUser: Codable {
-    
+    let _id: String
+    let firstName: String
+    let lastName: String
+    let email: String
+    let username: String
+    let password: String
+    let createdAt: String
+    let updatedAt: String
+    let __v: Int
 }
