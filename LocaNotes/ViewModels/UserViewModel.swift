79959913d88854ad31e35c580203f86348dd5edc
acomanzo <ansonih17@gmail.com>
2021-03-17 01:58:48 -0400

    made view model for user

--- LocaNotes/ViewModels/UserViewModel.swift
+++ LocaNotes/ViewModels/UserViewModel.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+public class UserViewModel: ObservableObject {
+    
+    // this user
+    @Published var user: User = User(id: -1, firstName: "", lastName: "", email: "", username: "", password: "", timeCreated: -1)
+    
+    func setUser(userId: Int32, firstName: NSString, lastName: NSString, email: NSString, username: NSString, password: NSString, timeCreated: Int32) {
+        user = User(id: userId, firstName: firstName, lastName: lastName, email: email, username: username, password: password, timeCreated: timeCreated)
+    }
+}
