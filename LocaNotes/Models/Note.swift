9526b400f14397dbb8897a4d9cbc89b473878c81
acomanzo <ansonih17@gmail.com>
2021-03-24 10:50:53 -0400

    add privacy id and note tag id to Note model

--- LocaNotes/Models/Note.swift
+++ LocaNotes/Models/Note.swift
@@ -13,6 +13,8 @@ import Foundation
 struct Note {
     let noteId: Int32
     let userId: Int32
+    let noteTagId: Int32
+    let privacyId: Int32
     let latitude: NSString
     let longitude: NSString
     let timeCreated: Int32 // unix
@@ -30,6 +32,8 @@ extension Note: SQLTable {
             CREATE TABLE Note(
                 NoteId INTEGER NOT NULL PRIMARY KEY,
                 UserId INTEGER NOT NULL,
+                NoteTagId INTEGER NOT NULL,
+                PrivacyId INTEGER NOT NULL,
                 Latitude TEXT NOT NULL,
                 Longitude TEXT NOT NULL,
                 TimeCreated INTEGER NOT NULL,
@@ -37,7 +41,9 @@ extension Note: SQLTable {
                 IsStory INT NOT NULL,
                 Upvotes INT NOT NULL,
                 Downvotes INT NOT NULL,
-                FOREIGN KEY(UserId) REFERENCES User(UserId)
+                FOREIGN KEY(UserId) REFERENCES User(UserId),
+                FOREIGN KEY(NoteTagId) REFERENCES NoteTag(NoteTagId),
+                FOREIGN KEY(PrivacyId) REFERENCES Privacy(PrivacyId)
             );
         """
     }
