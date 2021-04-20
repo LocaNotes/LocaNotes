4da4e8c6e6ccdd7350379d8dcec29fbe78c711b2
acomanzo <ansonih17@gmail.com>
2021-04-19 23:25:53 -0400

    privacy vm: queryAllFromStorage, queryPrivacyBy(serverId:), queryFromServer(completion:)

--- LocaNotes/ViewModels/PrivacyViewModel.swift
+++ LocaNotes/ViewModels/PrivacyViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+public class PrivacyViewModel {
+    private let privacyRepository: PrivacyRepository
+
+    public init() {
+        self.privacyRepository = PrivacyRepository()
+    }
+    
+    func queryAllFromStorage() -> [Privacy]? {
+        do {
+            return try privacyRepository.queryAllFromStorage()
+        } catch {
+            print("\(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    func queryPrivacyBy(serverId: String) -> Privacy? {
+        do {
+            return try privacyRepository.queryPrivacyBy(serverId: serverId)
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
+    func queryFromServer(completion: RESTService.RestResponseReturnBlock<[MongoPrivacyElement]>) {
+        privacyRepository.queryFromServer(completion: completion)
+    }
+}
