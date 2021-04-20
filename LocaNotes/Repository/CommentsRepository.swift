9e566c2c3b8d7b3e49ac61de037ff57c81305228
acomanzo <ansonih17@gmail.com>
2021-04-19 23:15:00 -0400

    queryCommentsFromServerBy(userId:completion:), insertComment, queryLocalCommentBy(serverId:)

--- LocaNotes/Repository/CommentsRepository.swift
+++ LocaNotes/Repository/CommentsRepository.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+public class CommentsRepository {
+    let sqliteDatabaseService: SQLiteDatabaseService
+    let restService: RESTService
+    
+    init() {
+//        self.sqliteDatabaseService = SQLiteDatabaseService()
+        self.sqliteDatabaseService = SQLiteDatabaseService.shared
+        self.restService = RESTService()
+    }
+    
+    func queryCommentsFromServerBy(userId: String, completion: RESTService.RestResponseReturnBlock<[MongoCommentElement]>) {
+        restService.queryCommentsFromServerBy(userId: userId, completion: completion)
+    }
+    
+    func insertComment(serverId: String, noteId: Int32, noteServerId: String, userId: Int32, userServerId: String, body: String, timeCommented: Int32) throws {
+        try sqliteDatabaseService.insertComment(serverId: serverId, noteId: noteId, noteServerId: noteServerId, userId: userId, userServerId: userServerId, body: body, timeCommented: timeCommented)
+    }
+    
+    func queryLocalCommentBy(serverId: String) throws -> Comment? {
+        try sqliteDatabaseService.queryLocalCommentBy(serverId: serverId)
+    }
+}
