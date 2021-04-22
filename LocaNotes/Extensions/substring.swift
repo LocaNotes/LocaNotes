83976295338b933f97073d8fda3281b68ae303b5
acomanzo <ansonih17@gmail.com>
2021-04-21 21:17:13 -0400

    created extension for substring

---
+++ LocaNotes/Extensions/substring.swift
@@ -0,0 +1,23 @@
+//
+//  substring.swift
+//  LocaNotes
+//
+//  Created by Anthony C on 4/21/21.
+//
+
+import Foundation
+
+extension String {
+    /**
+     Returns a substring up to the specified index of the specified string
+     - Parameters:
+        - startIndex: the starting index of the substring
+        - offset: the ending index of the substring
+     */
+    public func substring(start: Int = 0, offset: Int) -> String {
+        let startIndex = self.index(self.startIndex, offsetBy: start)
+        let endIndex = self.index(startIndex, offsetBy: offset - start)
+        let substring = self[startIndex..<endIndex]
+        return String(substring)
+    }
+}
