36cab2e0ed713cf21ba63114e0e1ba6851766b79
acomanzo <ansonih17@gmail.com>
2021-05-03 15:40:46 -0400

    wrote mongo friend

--- LocaNotes/Models/Mongo/MongoFriend.swift
+++ LocaNotes/Models/Mongo/MongoFriend.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+// MARK: - MongoFriendElement
+struct MongoFriendElement: Codable {
+    let id, userID, friendUserID, createdAt: String
+    let updatedAt: String
+    let v: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case userID = "userId"
+        case friendUserID = "friendUserId"
+        case createdAt, updatedAt
+        case v = "__v"
+    }
+}
